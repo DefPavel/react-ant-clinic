3f0d02da0edf920db93aab3fade66d1e1fce00a2
Evgenij Ryabko <jeniaryanko@mail.ru>
2023-07-01 18:18:26 +0300

    RemowedUseEffect

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Select, Button, Modal, Alert } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllUsers, AddUser } from '../../store/actions/users.action';
