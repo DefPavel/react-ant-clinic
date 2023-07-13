6619ab07d6f754bd6144a42e5af302ed3ed4bca6
EvgenijRyabko <jeniaryabko@bk.ru>
2023-07-14 01:33:41 +0300

    impovedReadability

--- src/utils/findDateSpace.js
+++ src/utils/findDateSpace.js
@@ -8,10 +8,10 @@ const convertToTime = (timefloat) => {
 export const findDateSpace = (timeArray = [], interval = 3600) => {
   for (let i = 8.0; i < 18.0; i += interval / 60 / 60) {
     if (
-      timeArray.every(
-        (el) =>
-          i >= parseFloat(el) + interval / 60 / 60 || i <= parseFloat(el) - interval / 60 / 60,
-      )
+      timeArray.every((el) => {
+        const floatTime = parseFloat(el);
+        return i >= floatTime + interval / 60 / 60 || i <= floatTime - interval / 60 / 60;
+      })
     )
       return convertToTime(i);
   }
